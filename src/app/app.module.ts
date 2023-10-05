59e81410980e744d3c6d431704876b2abf143edc
yigitalpkaynak <manwithtuned@gmail.com>
2023-10-05 19:11:24 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
+import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component';
+import { counterReducer } from './shared/store/counter.reducer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/Material.Module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterbuttonComponent,
+    CounterdisplayComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    StoreModule.forRoot({counter: counterReducer}),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
